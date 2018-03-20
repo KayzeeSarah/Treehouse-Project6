13f36f8cf060642de760b2667a0a408a2cfcf656
KayzeeSarah <36178374+KayzeeSarah@users.noreply.github.com>
2018-03-20 13:22:41 +0800

    final

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+const video = document.querySelector("video");
+const tscript = document.querySelectorAll(".tscript");
+
+video.addEventListener("timeupdate",function update() {
+
+  for(let i=0; i<tscript.length; i+=1){
+    let start = tscript[i].getAttribute("data-start");
+    let end = tscript[i].getAttribute("data-end");
+    if ( start<=video.currentTime && video.currentTime<=end) {
+        tscript[i].style.color = "#3A8753";
+    }else{
+        tscript[i].style.color = "#402E24";
+    }
+
+    tscript[i].addEventListener("click", function(e) {
+      let start = e.target.getAttribute("data-start");
+      video.currentTime = start;
+      video.play();
+    }, false);
+
+  }
+});
