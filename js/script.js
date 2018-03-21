8c02aab8adf3fc86ac838d01d3cfadec669dd177
KayzeeSarah <36178374+KayzeeSarah@users.noreply.github.com>
2018-03-21 10:06:42 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ video.addEventListener("timeupdate",function update() {
   for(let i=0; i<tscript.length; i+=1){
     let start = tscript[i].getAttribute("data-start");
     let end = tscript[i].getAttribute("data-end");
-    if ( start<=video.currentTime && video.currentTime<=end) {
+    if ( start<=video.currentTime && video.currentTime<end) {
         tscript[i].style.color = "#3A8753";
     }else{
         tscript[i].style.color = "#402E24";
